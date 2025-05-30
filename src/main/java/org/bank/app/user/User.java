97bd784d76ac63c4eea9901508b6a66a90254c37
Vitalii Chekalenko <chekalenkovitaliy@gmail.com>
2025-05-30 11:20:07 +0200

    [BApp] - services, beans, injection, logic

--- src/main/java/org/bank/app/user/User.java
+++ src/main/java/org/bank/app/user/User.java
@@ -1,4 +1,33 @@
 package org.bank.app.user;
 
+import org.bank.app.account.Account;
+
+import java.util.List;
+
 public class User {
+    private final int id;
+    private final String login;
+    private List<Account> accountList;
+
+    public User(int id, String login, List<Account> accountList) {
+        this.id = id;
+        this.login = login;
+        this.accountList = accountList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public List<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(List<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
